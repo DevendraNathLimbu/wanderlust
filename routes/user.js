38e32f94aa0a4294c40b11980efe8449594c8021
DevendraNathLimbu <srijansilimbolimbu@gmail.com>
2025-10-15 23:22:18 +0545

    error 1

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
-const wrapAsync = require('../utils/wrapAsync');
+const wrapAsync = require('../utils/wrapasync');
 const passport = require('passport');
 const { saveRedirectURL } = require('../middleware');
 const users = require('../controllers/users');
